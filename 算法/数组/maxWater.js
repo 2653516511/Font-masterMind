2d6815973c483741bd5c2af94c8cd39436bc6b71
admin <2653516511@qq.com>
2021-03-03 09:04:10 +0800

    f

--- 算法/数组/maxWater.js
+++ 算法/数组/maxWater.js
@@ -1,9 +1,19 @@
+const { reject } = require("../../../try/sourceUnder");
+
 /**
  * 接雨水
  * @param {array} arr 
  */
 const maxWater = (arr) => {
+    // 边界条件
+    if(arr.length <= 2) {
+        return 0
+    }
 
+    // 主逻辑
+    
+
+    // 返回值 number
 }
 const arr = [0,1,0,2,1,0,1,3,2,1,2,1]
 console.log(maxWater(arr));
