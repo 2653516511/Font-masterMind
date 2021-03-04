4c29576ca7d15e1b1d4da724c91edccfc3db9968
admin <2653516511@qq.com>
2021-03-04 15:23:34 +0800

    s

--- 算法/数组/promiseAll.js
+++ 算法/数组/promiseAll.js
@@ -3,7 +3,8 @@ function promiseAll(promiseArr) {
     return new Promise((resolve, reject) => {
         let res = [], count = 0
         for(let i = 0; i < promiseAll.length; ++i) {
-            promiseArr[i].then((v) => {
+            // promise.resolve(a)：会将a包装成一个promise
+            Promise.resolve(promiseArr[i]).then((v) => {
                 count++
                 res[i] = v
                 if(count === promiseArr.length) [
@@ -13,5 +14,20 @@ function promiseAll(promiseArr) {
                 reject(r)
             })
         }
+    })
+}
+function promiseRace(promiseArr) {
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(promiseArr)) {
+            return reject(new Error('请输入一个数组'))
+        }
+        for(let i = 0; i < promiseArr.length; ++i) {
+            Promise.resolve(promiseArr[i]).then((v) => {
+                resolve(v)
+            }, (r) => {
+                reject(r)
+            })
+        }
+
     })
 }
